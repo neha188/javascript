fcfed78870c8ffb6ecdbf8cf01449b36939563cc
neha188 <neha@vinsol.com>
2017-10-11 12:30:49 +0530

    chnaged as per suggestions

--- exercise1/custom.js
+++ exercise1/custom.js
@@ -1,21 +1,24 @@
-function user(name, age) {
+function User(name, age) {
   //Assigning values through constructor
   this.name = name;
   this.age = age;
 }
-user.prototype.compare = function(user) {
+User.prototype.compare = function(user) {
   if (this.age > user.age){
-    return  'john is older than mary';
+    return  this.name + ' is older than ' + user.name;
+  }
+  else if (this.age == user.age){
+    return  this.name + ' and ' + user.name + ' both are same age ';
   }
   else {
-    return  'mary is older than john';
+    return  user.name + ' is older than ' + this.name;
   }
 };
 //Creating First user instance
-var user1 = new user("john", 23);
+var user1 = new User("john", 23);
 
 //Creating Second user instance
-var user2 = new user("mary", 21);
+var user2 = new User("mary", 23);
 
 document.getElementById("demo").innerHTML = user1.compare(user2);
 
